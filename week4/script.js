242a6d1d10b600e8e805de861c2feacc8d3cc0e0
MandyChadwick <82726088+MandyChadwick@users.noreply.github.com>
2022-01-20 08:54:47 -0900

    working on chapter 8 notes incomplete

---
+++ week4/script.js
@@ -0,0 +1,14 @@
+const form = document.forms[0];
+console.log(form)
+document.write(form)
+const input = form.elements.searchInput;
+
+input.addEventListener('focus', () => alert('focused'),
+false);
+
+
+const input2 = form.elements.search2Input2;
+function search2(event) {
+    alert(`You Searched for: ${input2.value}`);
+    event.preventDefault();
+}
